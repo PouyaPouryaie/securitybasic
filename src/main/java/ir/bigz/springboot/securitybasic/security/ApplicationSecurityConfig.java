8e4b131ab5673e8c1a10bb0232eaec35341859e6
pouya <pouyapouryaie@gmail.com>
2020-07-19 18:45:42 +0430

    Title : add passwordEncoder
    App : security-basic
    
    Branch : userDetail
    
    Version: 1.0.1
    
    Date: 1399-04-29
    
    Auther: pouya pouryaie
    
    changes List:
    - create passwordEncoder
    - add passwordEncoder to the applicationSecurityConfig (this should be write when use userDetails Impl)
    
    ToDo List:

--- src/main/java/ir/bigz/springboot/securitybasic/security/ApplicationSecurityConfig.java
+++ src/main/java/ir/bigz/springboot/securitybasic/security/ApplicationSecurityConfig.java
@@ -1,13 +1,27 @@
 package ir.bigz.springboot.securitybasic.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
 
     @Override
@@ -20,5 +34,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+
+    /**
+     * this method just for test in memory used
+     * @return UserDetails
+     */
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails pouya = User.builder()
+                .username("pouya")
+                .password(passwordEncoder.encode("password"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(pouya);
     }
 }
